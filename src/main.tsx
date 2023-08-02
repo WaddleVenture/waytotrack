a35e87118e452a050db37200fade8aa8ac5f8ebd
WaddleVenture <138873196+WaddleVenture@users.noreply.github.com>
2023-08-02 16:10:17 +0200

    [+] Structuration and deleted useless files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// This main will call App.tsx that should be in src.
+// App.tsx should contains all of the pages ?
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/App.tsx
@@ -0,0 +1,22 @@
+import "./App.scss";
+
+// First take on the first page (it should be named something else)
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Let's track your games</h1>
+      <h1>and discover new ones along the way</h1>
+      <div className="buttons">
+        <p>
+          Track your games, preserve your gaming journey, and uncover new
+          adventures with our user-friendly application.
+        </p>
+        <button className="primary">Sign Up</button>
+        <button className="white">Learn More</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
